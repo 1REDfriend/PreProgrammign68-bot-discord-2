63cd21b924ef2acbc13ca78c8c2c013ca4bdbc4e
1REDfriend <83909061+1REDfriend@users.noreply.github.com>
2025-01-29 20:09:40 +0700

    c

--- bot/src/events/Client/onReady.js
+++ bot/src/events/Client/onReady.js
@@ -1,14 +1,10 @@
 const { success } = require("../../utils/Console");
 const Event = require("../../structure/Event");
-const { default: sendHeartbeat } = require("./HeartBeat");
 
 module.exports = new Event({
     event: 'ready',
     once: true,
     run: (__client__, client) => {
         success('Logged in as ' + client.user.displayName + ', took ' + ((Date.now() - __client__.login_timestamp) / 1000) + "s.")
-
-        sendHeartbeat(client)
-        setInterval(sendHeartbeat(client), 5 * 60 * 1000)
     }
 }).toJSON();
---
+++ bot/src/events/Client/onReadyHeatbeat.js
@@ -0,0 +1,35 @@
+const { PrismaClient } = require("@prisma/client")
+const Event = require("../../structure/Event")
+const { error } = require("../../utils/Console")
+const DiscordBot = require("../../client/DiscordBot")
+const prisma = new PrismaClient()
+
+module.exports = new Event({
+    event: 'ready',
+    once: false,
+
+
+    run: async (__client__, client) => {
+        /**
+        * 
+        * @param {DiscordBot} client 
+        */
+        async function sendHeartbeat(client) {
+            try {
+                await prisma.botAlive.create({
+                    data: {
+                        name: client.user.displayName,
+                        client_id: client.user.id,
+                    }
+                })
+            } catch (e) {
+                error(e)
+            } finally {
+                prisma.$disconnect()
+            }
+        }
+
+        await sendHeartbeat(client)
+        setInterval(() => sendHeartbeat(client), 5 * 60 * 1000)
+    }
+}).toJSON()
